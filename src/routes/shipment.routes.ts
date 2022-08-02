c9c44d69fddfe11929e29713f510908ccf2b6d40
Ricardo Vejarano <ricardo.vejarano@skillshare.com>
2022-08-02 18:12:13 -0500

    feat: inject shipment service in the route

--- src/routes/shipment.routes.ts
+++ src/routes/shipment.routes.ts
@@ -4,13 +4,19 @@ import { SERVICE_TYPES } from '../types';
 import { Logger } from '../core/logger';
 import { RouteDefinition } from '../interfaces/route.interface';
 import { inject, injectable } from 'inversify';
+import { ShipmentService } from '../services/shipment.service';
 
 @injectable()
 export class ShipmentRoutes implements RouteDefinition {
     private logger: winston.Logger;
+    private shipmentService: ShipmentService;
     
-    constructor(@inject(SERVICE_TYPES.Logger) winstonLogger: Logger) {
+    constructor(
+        @inject(SERVICE_TYPES.Logger) winstonLogger: Logger,
+        @inject(SERVICE_TYPES.ShipmentService) shipmentService: ShipmentService
+    ) {
         this.logger = winstonLogger.getLogger(`[${ShipmentRoutes.name}]`);
+        this.shipmentService = shipmentService;
     }
 
     public configureRoutes(app: express.Application): express.Application {
@@ -19,6 +25,7 @@ export class ShipmentRoutes implements RouteDefinition {
         });
         
         app.get('/shipments/:shipmentId', (req: any, res: any) => {
+            this.shipmentService.getShipmentById();
             res.send('Hello World from shipments!');
         });
 
